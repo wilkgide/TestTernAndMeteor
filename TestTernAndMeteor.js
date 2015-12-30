ba613a2873c653382c1854d27c3c9b31d8e58d9f
scp1 <gideon.squarecap@gmail.com>
2015-12-29 23:57:51 -0600

    Basic ToDo List
    Listing all Tasks in the Mongo db sorted in reverse chronological order
    Added ability to add a new task from the panel at the top of the screen.

--- TestTernAndMeteor.js
+++ TestTernAndMeteor.js
@@ -1,19 +1,29 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+Tasks = new Mongo.Collection("tasks");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+if(Meteor.isClient) {
+	Template.body.events({
+		"submit .new-task": function(event) {
+			//Prevent default browser form submit
+			event.preventDefault();
+			
+			var text = event.target.text.value;
+			
+			//Insert a task into the database
+			Tasks.insert({
+				text: text,
+				createdAt: new Date() //current time
+			})
+			
+			//clear form
+			event.target.text.value = "";
+		}
+	})
+	
+	Template.taskList.helpers({
+		tasks: function() {
+			return Tasks.find({}, {sort: {createdAt: -1}});
+		}
+	});
 }
 
 if (Meteor.isServer) {
