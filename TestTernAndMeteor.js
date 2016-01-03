ba4ac11e1513449918e6705ad250054c8393f702
scp1 <gideon.squarecap@gmail.com>
2016-01-02 21:25:02 -0600

    Add delete and checked to Tasks

--- TestTernAndMeteor.js
+++ TestTernAndMeteor.js
@@ -1,33 +1,54 @@
 Tasks = new Mongo.Collection("tasks");
 
-if(Meteor.isClient) {
+if (Meteor.isClient) {
 	Template.body.events({
-		"submit .new-task": function(event) {
-			//Prevent default browser form submit
+		"submit .new-task" : function(event) {
+			// Prevent default browser form submit
 			event.preventDefault();
-			
+
 			var text = event.target.text.value;
-			
-			//Insert a task into the database
+
+			// Insert a task into the database
 			Tasks.insert({
-				text: text,
-				createdAt: new Date() //current time
+				text : text,
+				createdAt : new Date()
+			// current time
 			})
-			
-			//clear form
+
+			// clear form
 			event.target.text.value = "";
 		}
 	})
-	
+
 	Template.taskList.helpers({
-		tasks: function() {
-			return Tasks.find({}, {sort: {createdAt: -1}});
+		tasks : function() {
+			return Tasks.find({}, {
+				sort : {
+					createdAt : -1
+				}
+			});
 		}
 	});
+
+	Template.task.events({
+		"click .toggle-checked" : function(event) {
+			// Set the checked property to the opposite of its current value
+			Tasks.update(this._id, {
+				$set : {
+					checked : !this.checked
+				}
+			});
+		},
+
+		"click .delete" : function(event) {
+			// Delete this task
+			Tasks.remove(this._id);
+		}
+	})
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+	Meteor.startup(function() {
+		// code to run on server at startup
+	});
 }
